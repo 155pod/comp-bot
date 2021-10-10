dc006d1d586743e128c73b29951a9983c1b0f061
benjamin wil <bw@benjaminwil.info>
2021-10-09 22:38:25 -0700

    Add new artist/track 'Now playing' formatter

--- songs.py
+++ songs.py
@@ -1,6 +1,7 @@
 import asyncio
 import itertools
 import random
+import re
 
 import discord
 
@@ -14,14 +15,38 @@ class Song:
         self.requester = source.requester
 
     def create_embed(self):
+        track_info = self.__nice_desc(self.source.title, self.source.url)
         embed = (discord.Embed(title='Now playing',
-                               description='```css\n{0.source.title}\n```'.format(self),
+                               description=track_info.format(self),
                                color=discord.Color.blurple())
-                 .add_field(name='Duration', value=self.source.duration)
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
-                 .set_thumbnail(url=self.source.thumbnail))
+        .add_field(name='Duration', value=self.source.duration)
+        .set_thumbnail(url=self.source.thumbnail))
 
         return embed
+
+    def __nice_desc(self, description_source, url):
+        description_elements = description_source.split(" - ")
+        separator_count = len(re.findall("\s\-\s", description_source))
+
+        # If there's only one separator found, we can be confident that we can
+        # format the artist and track title separately.
+        if separator_count == 1:
+            artist, title = description_elements
+        # If there's more than one separator found, we can naively split on
+        # the first separator and hope that's the right one to split on...
+        elif separator_count > 1:
+            artist = description_elements[:1]
+            title = " - ".join(description_elements[1:])
+        # In other cases there's no clear way to do anything cool. I'm scared
+        # of all the spiders. Catching things and eating their insides.
+        else:
+            artist = ""
+            title = description_source
+
+        if len(artist) > 0:
+            return f'{artist} - [{title}]({url})'
+        else:
+            return f'**[{title}]({url})**'
 
 class SongQueue(asyncio.Queue):
     def __getitem__(self, item):
